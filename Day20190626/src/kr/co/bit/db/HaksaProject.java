4639d838db94aeed811b93f7d97f003671ebb1bf
jisu <jsunfaded@gmail.com>
2019-06-26 19:10:54 +0900

    commit test

--- Day20190626/src/kr/co/bit/db/HaksaProject.java
+++ Day20190626/src/kr/co/bit/db/HaksaProject.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Scanner;
-//ªÏ∑¡¡‡...
+
 public class HaksaProject {
 
 	public static void main(String[] args) {
